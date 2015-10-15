76351844385785c1da2c41b39bd507eae3087252
Brian Hayden <bhayden@umn.edu>
2015-10-15 00:21:46 -0500

    Wow! Code! Docs!

---
+++ roi/README.md
@@ -0,0 +1,81 @@
+# The ROI-dentifier 0.1
+
+*author*: Brian Hayden <bdh@machinehum.com>
+originally based on objectmarker.cpp by: achu_wilson@rediffmail.com
+
+*Copyright 2015*, distributed under a *BSD license* without warranty. 
+
+# Summary 
+The object-marker iterates through a set of images and allows the user to mark 
+"Regions of Interest" (ROIs) for use downstream by OpenCV (etc.) training software. 
+
+# Requirements
+1. *nix, including Linux or OS X. There is no attempt at Windows compatibility at this time.
+2. OpenCV 3.
+3. C++, a sane C++0x compiler, and the standard libraries. 
+
+*Note:* This program is built for OpenCV 3, with the C++ API. Trying to mix it in with older versions,
+or the C API, is very likely to cause you pain. 
+
+# Build 
+1. Modify the Makefile as necessary for your environment. You'll need to supply the location
+   of your OpenCV headers (default is /usr/local/include/opencv), as well as the libraries if
+   they are somewhere other than /usr/local/lib. 
+2. make (and make install, if desired).
+
+# Usage
+
+`object-marker <input-directory> [params]`
+
+`<input-directory>`
+	The directory that object-marker will scan for images to be marked.
+	Defaults to the current working directory.
+
+`-m <marked-directory>`
+	The directory to which images will be moved if ROIs have been marked
+ 	on them ("Positive" images) *relative to* `<input-directory>`.
+	Defaults to <input-directory>/marked
+
+`-u <unmarked-directory>`
+  The directory to which images will be moved if ROIs have NOT been marked
+  on them ("Negative" images) *relative to* `<input-directory>`.
+  Defaults to <input-directory>/negative
+
+`-p <positive-file>`
+	The file to which positive file paths, and ROI data, will be written
+  *relative to* `<marked-directory>`. 
+	Defaults to <marked-directory>/pos.txt.
+
+`-n <negative-file>`
+	The file to which negative file paths will be written
+  *relative to* `<unmarked-directory>`. 
+	Defaults to <unmarked-directory>/neg.txt.
+
+`-h <max-height>`
+	The maximum height for the image display window. You want to use this if your 
+	display is smaller than your images to be marked -- OpenCV's display built-ins
+	aren't smart about scaling, or scrolling.
+
+`-x <aspect-ratio-width> -y <aspect-ratio-height>`
+	The default aspect ratio for the ROI selection tool is 1:1 (square), since this
+	is the default input ratio for programs like opencv_createsamples. If you will
+	be using a different aspect ratio downstream (such as the 1:2 common with HoG
+	training for figures), supply that here. 
+
+# Operation 
+- Click the mouse to set a starting corner for the ROI. 
+- As you move the mouse, you'll see a red rectangle locked to the current aspect ratio. 
+- Click a second time to define the opposite corner of the ROI. 
+- The rectangle will now turn green to indicate that it is set, and ready for you to 
+  confirm. 
+- To confirm and record this ROI, press `<space bar>`.
+- When you are done selecting ROIs on the image, press *b* to move to the next image. 
+  This also triggers the ROIs, if any, to be written to file, and moves the image to
+  either the "marked" or "negative" directory (depending on whether any ROIs were
+  marked). 
+- Continue until all images are marked, or press `<escape>` to quit. Because images are
+  moved out of the input directory once they are dealt with, and the positive/negative
+  output files are appended to rather than overwritten, you may quit at any time and 
+  pick up where you left off by simply launching object-marker with the same parameters.
+
+Have fun!
---
+++ roi/roi.cpp
@@ -0,0 +1,281 @@
+/***************objectmarker.cpp******************
+
+Object marker for creating a list of ROIs to use for (e.g.) OpenCV cascade training. 
+
+Requires OpenCV, and various common C/C++ libraries. 
+
+author: bdh@machinehum.com
+originally based on objectmarker.cpp by: achu_wilson@rediffmail.com
+*/
+
+#include <opencv/cv.h>
+#include <opencv/cvaux.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+#include <stdio.h>
+#include <sys/uio.h>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+using namespace cv;
+using namespace std;
+
+Mat image;
+Mat image2;
+Mat image3;
+float max_height = 800;
+float scale_factor = 1;
+int roi_x0=0;
+int roi_y0=0;
+int roi_x1=0;
+int roi_y1=0;
+int numOfRec=0;
+int startDraw = 0;
+int aspect_x = 1;
+int aspect_y = 1;
+int mod_x = 0;
+int mod_y = 0;
+string window_name="";
+
+void on_mouse(int event,int x,int y,int flag, void *param)
+{
+    if ( event == CV_EVENT_LBUTTONDOWN )
+    {
+        if ( !startDraw )
+        {
+          roi_x0=x;
+          roi_y0=y;
+          startDraw = 1;
+        } else {
+          roi_x1=x;
+          roi_y1=y;
+
+          // enforce ROI aspect ratio
+          if (
+               ( (roi_x0 > x) && (roi_y0 > y) )
+               ||
+               ( (roi_x0 < x) && (roi_y0 < y) )
+             )
+            roi_x1 = roi_x0 + ( aspect_x * ( roi_y1 - roi_y0 ) / aspect_y );
+          else
+            roi_x1 = roi_x0 + ( aspect_x * ( roi_y0 - roi_y1 ) / aspect_y );
+
+          // redraw ROI selection in green when finished
+          image2 = image3.clone();
+          rectangle( image2, cvPoint( roi_x0, roi_y0 ), cvPoint( roi_x1, roi_y1 ), CV_RGB(50,255,50), 1 );
+          imshow( window_name, image2 );
+
+          startDraw = 0;
+        }
+    }
+    if ( event == CV_EVENT_MOUSEMOVE && startDraw )
+    {
+        mod_x = x; 
+        mod_y = y;
+
+        // enforce ROI aspect ratio
+        if (  
+             ( (roi_x0 > x) && (roi_y0 > y) )
+             ||
+             ( (roi_x0 < x) && (roi_y0 < y) )
+           )
+          mod_x = roi_x0 + ( aspect_x * ( mod_y - roi_y0 ) / aspect_y );
+        else
+          mod_x = roi_x0 + ( aspect_x * ( roi_y0 - mod_y ) / aspect_y );
+
+        //redraw ROI selection
+        image2 = image3.clone();
+        rectangle(image2,cvPoint( roi_x0, roi_y0 ),cvPoint( mod_x, mod_y ),CV_RGB(255,0,100),1);
+        imshow(window_name,image2);
+    }
+
+}
+
+int main(int argc, char** argv)
+{
+  char iKey=0;
+  string strPrefix;
+  string strPostfix;
+  string fullPath;
+  string destPath;
+
+  string input_directory = ".";
+  string pos_directory = "marked";
+  string neg_directory = "negative";
+  string positive_file = "pos.txt";
+  string negative_file = "neg.txt";
+
+  struct stat filestat;
+  int c;
+
+  while ((c = getopt (argc, argv, "m:u:p:n:h:x:y:")) != -1)
+    switch (c)
+      {
+      case 'm':
+        pos_directory = optarg;
+        break;
+      case 'u':
+        neg_directory = optarg;
+        break;
+      case 'p':
+        positive_file = optarg;
+        break;
+      case 'n':
+        negative_file = optarg;
+        break;
+      case 'h':
+        max_height = std::stod(optarg);
+        break;
+      case 'x':
+        aspect_x = std::stoi(optarg);
+        break;
+      case 'y':
+        aspect_y = std::stoi(optarg);
+        break;
+      case '?':
+        if (optopt == 'd' || optopt == 'p' || optopt == 'n')
+          fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+        else if (isprint (optopt))
+          fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+        else
+          fprintf (stderr,
+                   "Unknown option character `\\x%x'.\n",
+                   optopt);
+        return 1;
+      default:
+        abort ();
+      }
+
+  if ( argv[optind] != NULL ) 
+    input_directory = argv[optind];
+
+  pos_directory = input_directory + "/" + pos_directory;
+  neg_directory = input_directory + "/" + neg_directory;
+  positive_file = pos_directory + "/" + positive_file;
+  negative_file = neg_directory + "/" + negative_file;
+
+  /* Create output directories if necessary */
+  DIR *dir_d = opendir( pos_directory.c_str());
+  if ( dir_d == NULL ) {
+    mkdir( pos_directory.c_str(), S_IRWXU | S_IRWXG );
+  } else {
+    closedir(dir_d);
+  }
+
+  DIR *dir_e = opendir( neg_directory.c_str());
+  if ( dir_e == NULL ) {
+    mkdir( neg_directory.c_str(), S_IRWXU | S_IRWXG );
+  } else {
+    closedir(dir_e);
+  }
+
+  // Open input directory 
+  DIR *dir_p = opendir( input_directory.c_str() );
+  struct dirent *dir_entry_p;
+
+  if(dir_p == NULL) {
+    fprintf(stderr, "Failed to open directory %s\n", input_directory.c_str());
+    return -1;
+  }
+
+  // GUI 
+  window_name = "ROI Marking";
+  namedWindow(window_name,1);
+  setMouseCallback(window_name,on_mouse, NULL);
+
+  // init output streams
+  ofstream positives(positive_file.c_str(), std::ofstream::out | std::ofstream::app);
+  ofstream negatives(negative_file.c_str(), std::ofstream::out | std::ofstream::app);
+
+  // Iterate over directory 
+  while((dir_entry_p = readdir(dir_p)) != NULL)
+  {
+    // Skip directories
+    string relPath = input_directory + "/" + dir_entry_p->d_name;
+    if ( stat( relPath.c_str(), &filestat ) == -1 ) continue;
+    if (S_ISDIR( filestat.st_mode )) continue;
+
+    strPostfix="";
+    numOfRec=0;
+
+    if(strcmp(dir_entry_p->d_name, ""))
+      fprintf(stderr, "Examining file %s\n", dir_entry_p->d_name);
+
+    strPrefix = dir_entry_p->d_name;
+    fullPath  = input_directory + "/" + strPrefix;
+    printf("Loading image %s\n", strPrefix.c_str());
+
+    image = imread(fullPath.c_str(),1);
+
+    if(! image.data )                              // Check for invalid input
+    {
+        cout <<  "Could not open or find the image" << std::endl ;
+        continue;
+    }
+
+    do
+    {
+      // Use scratch version of image for display, scaling down if necessary.
+      Size sz = image.size();
+      if ( sz.height > max_height ) {
+        if (sz.width > sz.height) {
+          scale_factor = max_height / sz.width;
+        } else {
+          scale_factor = max_height / sz.height;
+        }
+        resize(image, image3, Size(), scale_factor, scale_factor, INTER_AREA);
+      } else {
+        image3 = image.clone();
+      }
+      imshow(window_name,image3);
+
+      // Start taking input
+      iKey=cvWaitKey(0);
+      switch(iKey)
+      {
+        case 27:
+          image.release();
+          image2.release();
+          image3.release();
+          destroyWindow(window_name);
+          closedir(dir_p);
+          return 0;
+        case 32:
+          numOfRec++;
+#ifdef DEBUG
+          printf("   %d. rect x=%d\ty=%d\tx2h=%d\ty2=%d\n",numOfRec,roi_x0,roi_y0,roi_x1,roi_y1);
+#endif
+	  strPostfix += " " + to_string( int( min(roi_x0,roi_x1) / scale_factor ) ) + " " + 
+                              to_string( int( min(roi_y0,roi_y1) / scale_factor ) ) + " " +
+                              to_string( int( ( abs(roi_x1 - roi_x0) ) / scale_factor ) ) + " " + 
+                              to_string( int( ( abs(roi_y1 - roi_y0) ) / scale_factor ) );
+        break;
+      }
+    }
+    while(iKey!=98);
+
+    if (iKey==98) {
+      if (numOfRec > 0) {
+        positives << strPrefix << " "<< numOfRec << strPostfix <<"\n";
+        destPath = pos_directory + "/" + strPrefix;  
+      } else {
+        negatives << strPrefix << "\n";
+        destPath = neg_directory + "/" + strPrefix;
+      }
+    }
+    rename( fullPath.c_str(), destPath.c_str() );
+  }
+
+  image.release();
+  image2.release();
+  image3.release();
+  destroyWindow(window_name);
+  closedir(dir_p);
+  return 0;
+}
